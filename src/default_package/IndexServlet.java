4279946ee8f49547925dc9238ed05e0939bbfd71
Gackt8 <55741761+Gackt8@users.noreply.github.com>
2020-12-06 00:47:53 +0300

    Update IndexServlet.java

--- src/default_package/IndexServlet.java
+++ src/default_package/IndexServlet.java
@@ -18,9 +18,24 @@ public class IndexServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        ArrayList<Book> products = BookDB.select();
-        request.setAttribute("products", products);
+
 
         getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+
+            String nameAuthor = request.getParameter("nameAuthor");
+            ArrayList<Book> search = BookDB.selectAuthor(nameAuthor);
+            request.setAttribute("search", search);
+
+            getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+        }
+        catch(Exception ex) {
+            getServletContext().getRequestDispatcher("/notfound.jsp").forward(request, response);
+        }
     }
 }
