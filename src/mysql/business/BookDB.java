93d761725be5741710a36f59c34456892357e04e
Gackt8 <55741761+Gackt8@users.noreply.github.com>
2020-12-06 00:46:38 +0300

    Update BookDB.java

--- src/mysql/business/BookDB.java
+++ src/mysql/business/BookDB.java
@@ -8,31 +8,64 @@ public class BookDB {
     private static String username = "root";
     private static String password = "jhj5655565";
 
-    public static ArrayList<Book> select() {
+//     public static ArrayList<Book> select() {
 
-        ArrayList<Book> products = new ArrayList<Book>();
+//         ArrayList<Book> products = new ArrayList<Book>();
+//         try{
+//             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+//             try (Connection conn = DriverManager.getConnection(url, username, password)){
+
+//                 Statement statement = conn.createStatement();
+//                 ResultSet resultSet = statement.executeQuery("SELECT * FROM book");
+//                 while(resultSet.next()){
+
+//                     int id = resultSet.getInt(1);
+//                     String authorName = resultSet.getString(2);
+//                     int year = resultSet.getInt(3);
+//                     String text = resultSet.getString(4);
+
+//                     Book book = new Book(id, authorName, year, text);
+//                     products.add(book);
+//                 }
+//             }
+//         }
+//         catch(Exception ex){
+//             System.out.println(ex);
+//         }
+//         return products;
+//     }
+    
+     public static ArrayList<Book> selectAuthor(String nameAuthor) throws ClassNotFoundException {
+
+        ArrayList<Book> booksAuthors = new ArrayList<Book>();
         try{
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             try (Connection conn = DriverManager.getConnection(url, username, password)){
 
-                Statement statement = conn.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM book");
+                String sql = "SELECT * FROM book WHERE AuthorName=?";
+                try(PreparedStatement preparedStatement = conn.prepareStatement(sql)){
+                    preparedStatement.setString(1, nameAuthor);
+
+                    ResultSet resultSet = preparedStatement.executeQuery();
+
                 while(resultSet.next()){
 
                     int id = resultSet.getInt(1);
                     String authorName = resultSet.getString(2);
                     int year = resultSet.getInt(3);
+
                     String text = resultSet.getString(4);
 
                     Book book = new Book(id, authorName, year, text);
-                    products.add(book);
+                    booksAuthors.add(book);
                 }
             }
+        }
         }
         catch(Exception ex){
             System.out.println(ex);
         }
-        return products;
+        return booksAuthors;
     }
 
     public static Book selectOne(int id) {
@@ -91,12 +124,12 @@ public class BookDB {
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             try (Connection conn = DriverManager.getConnection(url, username, password)){
 
-                String sql = "UPDATE book SET AuthorName = ?, YearOfBook = ?, Text = ? WHERE Id = ?";
+                String sql = "UPDATE book SET YearOfBook = ?, Text = ? WHERE Id = ?";
                 try(PreparedStatement preparedStatement = conn.prepareStatement(sql)){
-                    preparedStatement.setString(1, book.getName());
-                    preparedStatement.setInt(2, book.getYear());
-                    preparedStatement.setString(3, book.getText());
-                    preparedStatement.setInt(4, book.getId());
+                  
+                    preparedStatement.setInt(1, book.getYear());
+                    preparedStatement.setString(2, book.getText());
+                    preparedStatement.setInt(3, book.getId());
 
                     return  preparedStatement.executeUpdate();
                 }
